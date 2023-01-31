3049e5b7c872f7c77e09529ef3e0842a561ac71b
yohn.zhu <yohn.zhu@ucloud.cn>
2023-01-31 15:54:03 +0800

    add merger

---
+++ WzMerger/Merger.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using MapleLib.WzLib;
+using MapleLib.WzLib.Util;
+
+namespace WzMerger
+{
+    public class Merger
+    {
+        WzFile baseFile;
+        WzFile overrideFile;
+
+        public Merger(string baseWz, string overrideWz)
+        {
+            this.baseFile = loadWzFile(baseWz);
+            this.overrideFile = loadWzFile(overrideWz);
+        }
+
+        private WzFile loadWzFile(string wzPath)
+        {
+            short fileVersion = 0;
+            var gameVersion = WzTool.DetectMapleVersion(wzPath, out fileVersion);
+            WzFile f = new WzFile(wzPath, fileVersion, gameVersion);
+            var parseResult = f.ParseWzFile();
+            if (parseResult == WzFileParseStatus.Success)
+            {
+                return f;
+            }
+            else
+            {
+                throw new Exception(WzFileParseStatusExtension.GetErrorDescription(parseResult));
+            }
+
+        }
+
+        public void Merge(string path)
+        {
+            var baseObj = this.baseFile.GetObjectFromPath(path, true);
+            if (baseObj == null)
+            {
+                throw new Exception("base file does not have this object, skip");
+            }
+            var overrideObj = this.overrideFile.GetObjectFromPath(path, true);
+            if (overrideObj == null)
+            {
+                throw new Exception("override file does not have this object, skip");
+            }
+            replaceObject(baseObj, overrideObj);
+        }
+
+        public void Save(string path)
+        {
+            this.baseFile.SaveToDisk(path, true, this.baseFile.MapleVersion);
+        }
+
+        void replaceObject(WzObject wz, WzObject wz1)
+        {
+            if (wz.ObjectType != wz1.ObjectType)
+            {
+                throw new Exception("needed merge file had different type");
+            }
+            if (compareObject(wz, wz1))
+            {
+                Console.WriteLine("same object, skip");
+                return;
+            }
+            switch (wz.ObjectType)
+            {
+                case WzObjectType.Directory:
+                    replaceDirectory((WzDirectory)wz, (WzDirectory)wz1);
+                    break;
+                case WzObjectType.Image:
+                    replaceImage((WzImage)wz, (WzImage)wz1);
+                    break;
+                case WzObjectType.Property:
+                    replaceProperty((WzImageProperty)wz, (WzImageProperty)wz1);
+                    break;
+                default:
+                    throw new Exception("not supported");
+            }
+        }
+
+        bool compareObject(WzObject wz, WzObject wz1)
+        {
+            if (wz.ObjectType != wz1.ObjectType)
+            {
+                throw new Exception("needed merge file had different type");
+            }
+            switch (wz.ObjectType)
+            {
+                case WzObjectType.Directory:
+                    return compareDirectory((WzDirectory)wz, (WzDirectory)wz1);
+                case WzObjectType.Image:
+                    return compareImage((WzImage)wz, (WzImage)wz1);
+                case WzObjectType.Property:
+                    return compareProperty((WzImageProperty)wz, (WzImageProperty)wz1);
+                default:
+                    throw new Exception("not supported");
+            }
+        }
+
+        void replaceDirectory(WzDirectory dir1, WzDirectory dir2)
+        {
+            if (dir1.Name != dir2.Name)
+            {
+                throw new Exception("needed merge file had different name");
+            }
+            var subDirs1 = dir1.WzDirectories.ToDictionary(x => x.Name);
+            var subDirs2 = dir2.WzDirectories.ToDictionary(x => x.Name);
+            foreach (var key in subDirs1.Keys)
+            {
+                if (!subDirs2.ContainsKey(key) || compareDirectory(subDirs1[key], subDirs2[key]))
+                {
+                    continue;
+                }
+                replaceDirectory(subDirs1[key], subDirs2[key]);
+            }
+            foreach (var key in subDirs2.Keys)
+            {
+                if (!subDirs1.ContainsKey(key))
+                {
+                    dir1.AddDirectory(subDirs2[key]);
+                }
+            }
+
+            var subImgs1 = dir1.WzImages.ToDictionary(x => x.Name);
+            var subImgs2 = dir2.WzImages.ToDictionary(x => x.Name);
+            foreach (var key in subImgs1.Keys)
+            {
+                if (!subImgs2.ContainsKey(key) || compareImage(subImgs1[key], subImgs2[key]))
+                {
+                    continue;
+                }
+                replaceImage(subImgs1[key], subImgs2[key]);
+            }
+            foreach (var key in subImgs2.Keys)
+            {
+                if (!subImgs1.ContainsKey(key))
+                {
+                    dir1.AddImage(subImgs2[key]);
+                }
+            }
+        }
+
+        bool compareDirectory(WzDirectory dir1, WzDirectory dir2)
+        {
+            if (dir1.Name != dir2.Name)
+            {
+                return false;
+            }
+            if (dir1.WzDirectories.Count != dir2.WzDirectories.Count)
+            {
+                return false;
+            }
+            var subDirs1 = dir1.WzDirectories.ToDictionary(x => x.Name);
+            var subDirs2 = dir2.WzDirectories.ToDictionary(x => x.Name);
+            foreach (var key in subDirs1.Keys)
+            {
+                if (!subDirs2.ContainsKey(key))
+                {
+                    return false;
+                }
+                if (!compareDirectory(subDirs1[key], subDirs2[key]))
+                {
+                    return false;
+                }
+            }
+
+            if (dir1.WzImages.Count != dir2.WzImages.Count)
+            {
+                return false;
+            }
+            var subImgs1 = dir1.WzImages.ToDictionary(x => x.Name);
+            var subImgs2 = dir2.WzImages.ToDictionary(x => x.Name);
+            foreach (var key in subImgs1.Keys)
+            {
+                if (!subImgs2.ContainsKey(key))
+                {
+                    return false;
+                }
+                if (!compareImage(subImgs1[key], subImgs2[key]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        bool compareImage(WzImage img1, WzImage imgs)
+        {
+            if (img1.Name != imgs.Name)
+            {
+                return false;
+            }
+            if (img1.WzProperties.Count != imgs.WzProperties.Count)
+            {
+                return false;
+            }
+            var dict1 = img1.WzProperties.ToDictionary(x => x.Name);
+            var dict2 = imgs.WzProperties.ToDictionary(x => x.Name);
+            foreach (var key in dict1.Keys)
+            {
+                if (!dict2.ContainsKey(key))
+                {
+                    return false;
+                }
+                if (!compareProperty(dict1[key], dict2[key]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        void replaceImage(WzImage img1, WzImage imgs)
+        {
+            if (img1.Name != imgs.Name)
+            {
+                throw new Exception("name not match");
+            }
+            if (img1.WzProperties.Count != imgs.WzProperties.Count)
+            {
+                throw new Exception("property count not match");
+            }
+            var dict1 = img1.WzProperties.ToDictionary(x => x.Name);
+            var dict2 = imgs.WzProperties.ToDictionary(x => x.Name);
+            foreach (var key in dict1.Keys)
+            {
+                if (!dict2.ContainsKey(key) || compareProperty(dict1[key], dict2[key]))
+                {
+                    continue;
+                }
+                replaceProperty(dict1[key], dict2[key]);
+            }
+            foreach (var key in dict2.Keys)
+            {
+                if (!dict1.ContainsKey(key))
+                {
+                    img1.AddProperty(dict2[key]);
+                }
+            }
+        }
+
+        bool compareProperty(WzImageProperty prop1, WzImageProperty prop2)
+        {
+            if (prop1.Name != prop2.Name)
+            {
+                return false;
+            }
+            if (prop1.PropertyType != prop2.PropertyType)
+            {
+                return false;
+            }
+            if (prop1.WzValue != prop2.WzValue)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        void replaceProperty(WzImageProperty prop1, WzImageProperty prop2)
+        {
+            if (prop1.Name != prop2.Name)
+            {
+                throw new Exception("name not match");
+            }
+            if (prop1.PropertyType != prop2.PropertyType)
+            {
+                throw new Exception("type not match");
+            }
+            if (prop1.PropertyType != WzPropertyType.String)
+            {
+                return;
+            }
+            if (prop1.WzValue != prop2.WzValue)
+            {
+                prop1.SetValue(prop2.WzValue);
+            }
+        }
+    }
+
+
+}
+
---
+++ WzMerger/Program.cs
@@ -0,0 +1,53 @@
+﻿using WzMerger;
+
+static void Run()
+{
+    Console.WriteLine("Input the source wz file you want to use");
+    string? sourceWzPath = Console.ReadLine();
+    if (sourceWzPath == null)
+    {
+        Console.WriteLine("empty file path");
+        return;
+    }
+    Console.WriteLine("Input the override wz file you want to use");
+    string? overrideWzPath = Console.ReadLine();
+    if (overrideWzPath == null)
+    {
+        Console.WriteLine("empty file path");
+        return;
+    }
+
+    try
+    {
+        Merger merger = new Merger(sourceWzPath, overrideWzPath);
+        Console.WriteLine("wz file loaded");
+        Console.WriteLine("Input the path you want to merge");
+        string? path = Console.ReadLine();
+        if (path == null)
+        {
+            Console.WriteLine("empty path");
+            return;
+        }
+        merger.Merge(path);
+        Console.WriteLine("merge finished");
+        Console.WriteLine("Input the path you want to save");
+        string? savePath = Console.ReadLine();
+        if (savePath == null)
+        {
+            Console.WriteLine("empty path");
+            return;
+        }
+        merger.Save(savePath);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("wz file merge failed :" + ex.Message);
+        return;
+    }
+}
+
+
+Run();
+Console.WriteLine("Press any key...");
+Console.ReadKey();
+
